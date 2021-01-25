9f76075271c8400aea18df00afc084c454ecb1ee
Alec Girman <avgch1@gmail.com>
2021-01-25 03:07:53 +0000

    zsh stuff updated

--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -1,7 +1,7 @@
 # connect to 2410vandyke wifi
 function wifi-connect() {
     # 1. Authenticate with network
-    wpa_supplicant -i wlp1s0 -c /etc/wpa_supplicant/2410vandyke.conf &
+    wpa_supplicant -i wlp5s0 -c /etc/wpa_supplicant/wifi.conf &
 
     # wait for wifi to connect before starting the dhcp client daemon
     sleep 3
@@ -10,18 +10,6 @@ function wifi-connect() {
     dhcpcd
 
     sleep 2
-
-    # conduct ping tset to check for successful connection
-    echo "Conducting ping test..."
-    ping www.google.com &> /dev/null
-
-    if [[ $? -eq 2 ]]
-    then
-        echo "Failed to connect"
-    elif [[ $? -eq 0 ]]
-    then
-        echo "Connection successful!"
-    fi
 }
 
 function start-kde() {
